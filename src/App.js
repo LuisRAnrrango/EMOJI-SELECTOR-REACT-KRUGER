d17e9d941bcb5dd1ef34a5f01afd2597fb7dd8f1
Luisito <luisitoylag@gmail.com>
2023-01-07 14:03:39 -0500

    Agregando Bootstrap, Route, mejorando Navegación en la aplicación

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
-import EmojiPickerInput from "./components/emojiPicker/EmojiPickerInput";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Home } from "./components/pages/Home";
+import { Emojin } from "./components/pages/Emojin";
+import { About } from "./components/pages/About";
+import { Contact } from "./components/pages/Contact";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-     <EmojiPickerInput></EmojiPickerInput>
-    </div>
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/chat" element={<Emojin />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/Emojin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import EmojiPickerInput from "../emojiPicker/EmojiPickerInput";
+
+export const Emojin = () => {
+  return (
+    <div>
+      <div>Emojin</div>
+      <EmojiPickerInput></EmojiPickerInput>
+    </div>
+  );
+};
